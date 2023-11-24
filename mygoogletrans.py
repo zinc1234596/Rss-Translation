57ef2072990836657fc2a02f3edcec3d95209a36
去年夏天 <tjsky@foxmail.com>
2023-11-24 10:44:15 +0800

    转义link与guid内的&以符合XML格式

--- mygoogletrans.py
+++ mygoogletrans.py
@@ -195,6 +195,9 @@ def tran(sec):
             .replace('"', "&quot;")
             .replace("'", "&#39;")
         )
+        转义link与guid内的&以符合XML格式
+        link = link.replace("&", "&amp;")
+        guid = guid.replace("&", "&amp;")
         one = dict(
             title=title, link=link, description=description, guid=guid, pubDate=pubDate
         )
